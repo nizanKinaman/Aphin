d1706311df584ede61680ab0b37e7020a8d3c2a8
Nikita <nik_sikora@mail.com>
2021-10-12 16:17:31 +0300

    Добавьте файлы проекта.

---
+++ Aphin/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Aphin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(927, 706);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(976, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 29);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ребро";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(976, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 29);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Полигон";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(976, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(976, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1146, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "смещение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(976, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 736);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Aphin/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aphin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g.Clear(Color.White);
+        }
+
+        int X0 = 0;
+        int Y0 = 0;
+
+        int X1 = 0;
+        int Y1 = 0;
+
+        static Bitmap bmp = new Bitmap(927, 706);
+
+        bool mouse_Down = false;
+
+        public Graphics g = Graphics.FromImage(bmp);
+
+        Pen myPen = new Pen(Color.Black, 3f);
+
+        Pen pen_fill = new Pen(Color.Black);
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_Down = true;
+            bmp.SetPixel(e.X, e.Y, Color.Black);
+            this.X0 = e.X;
+            this.Y0 = e.Y;
+            pictureBox1.Image = bmp;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            g.Clear(Color.White);
+            this.X1 = Math.Min(pictureBox1.Width - 1, Math.Max(e.X, 1));
+            this.Y1 = Math.Min(pictureBox1.Height - 1, Math.Max(e.Y, 1));
+            if (mouse_Down && radioButton1.Checked)
+                Line_Bres(X0, Y0, X1,Y1);
+            else
+                if (mouse_Down && radioButton2.Checked)
+                Draw_square(X0, Y0, X1, Y1);
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_Down = false;
+
+        }
+
+        static void Swap(ref int x, ref int y)
+        {
+            int t = x;
+            x = y;
+            y = t;
+        }
+        void Line_Bres(int x0, int y0, int x1, int y1)
+        {
+            int xtemp0 = x0;
+            int xtemp1 = x1;
+            int ytemp0 = y0;
+            int ytemp1 = y1;
+            if (Math.Abs(x1 - x0) < Math.Abs(y1 - y0))
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+            int deltax = Math.Abs(x1 - x0);
+            int deltay = Math.Abs(y1 - y0);
+            int error = 0;
+            int deltaerr = (deltay + 1);
+            int y = y0;
+            int diry = y1 - y0;
+            if (diry > 0)
+                diry = 1;
+            if (diry < 0)
+                diry = -1;
+            for (int x = x0; x <= x1; x++)
+            {
+                bmp.SetPixel(Math.Abs(ytemp1 - ytemp0) > Math.Abs(xtemp1 - xtemp0) ? y : x, Math.Abs(ytemp1 - ytemp0) > Math.Abs(xtemp1 - xtemp0) ? x : y, Color.Black);
+                error = error + deltaerr;
+                if (error >= deltax + 1)
+                {
+                    y += diry;
+                    error = error - (deltax + 1);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        void Draw_square(int x0, int y0, int x1, int y1)
+        {
+            int xtemp0 = x0;
+            int xtemp1 = x1;
+            int ytemp0 = y0;
+            int ytemp1 = y1;
+
+            if (xtemp1 + xtemp0 > ytemp1 + ytemp0)
+            {
+                //xtemp1 = xtemp0 + Math.Abs(ytemp1 - ytemp0);
+            }
+            else
+            if (xtemp1 + xtemp0 < ytemp1 + ytemp0)
+            {
+              // ytemp1 = ytemp0 + Math.Abs(xtemp1 - xtemp0);
+            }
+
+            //if(xtemp1>xtemp0 && ytemp1>xtemp0)
+
+            Line_Bres(xtemp0, ytemp0, xtemp0, ytemp1);
+            Line_Bres(xtemp0, ytemp1, xtemp1, ytemp1);
+            Line_Bres(xtemp0, ytemp0, xtemp1, ytemp0);
+            Line_Bres(xtemp1, ytemp0, xtemp1, ytemp1);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Image = bmp;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            int dx = int.Parse(textBox1.Text);
+            int dy = int.Parse(textBox2.Text);
+            //MessageBox.Show(dx + " " + dy);
+            //X0 = Math.Min(pictureBox1.Width - 1, Math.Max(X0-dx, 1));
+            //X1 = Math.Min(pictureBox1.Width - 1, Math.Max(X1-dx, 1)); 
+            //Y0 = Math.Min(pictureBox1.Height - 1, Math.Max(Y1-dy, 1)); 
+            //Y1 = Math.Min(pictureBox1.Height - 1, Math.Max(Y1-dy, 1));
+            if (radioButton1.Checked)
+                Line_Bres(X0, Y0, X1, Y1);
+            else
+               if (radioButton2.Checked)
+                Draw_square(X0, Y0, X1, Y1);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number))
+                e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number))
+                e.Handled = true;
+        }
+    }
+}
